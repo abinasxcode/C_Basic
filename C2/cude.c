f7b245dfa1b6d33cb39cd625af615cbccaa058b0
abinasxcode <abinashkundu8@gmail.com>
2023-11-01 21:38:04 +0530

    cube rule

---
+++ C2/cude.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    int number;
+
+    printf("\n \t Enter  a number:");
+    scanf("%d",&number);
+    printf("\n\t Cude of number is:%d""\n\n",number*number*number);
+    return 0;
+}
