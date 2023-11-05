eea9aaee201a9d402a218a9f264789b201dd1952
abinasxcode <abinashkundu8@gmail.com>
2023-11-05 11:15:17 +0530

    code no 15

---
+++ C15/C15.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int hexadeciaml=0x2A;//Hexadecimal representation of decimal 42
+
+    printf(" The hexadecimal contant is:%x\n",hexadeciaml);
+
+    return 0;
+}
