f5d12620edbd54f66e5d1f678e5a189cf65b6232
abinasxcode <abinashkundu8@gmail.com>
2023-11-07 18:55:22 +0530

    code no 19

---
+++ C19/C19.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+
+    int i;
+    printf("Enter value of i");
+    scanf("%d",&i);
+    if (i=5)
+    printf("you entered 5\n");
+    else 
+    printf("you entered something other than 5\n");
+
+    return 0;
+}
