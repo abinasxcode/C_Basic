b63b4d5e77d5e6f20894af711cc8c2d85737d305
abinasxcode <abinashkundu8@gmail.com>
2023-12-06 18:39:36 +0530

    test no 24

---
+++ C24/C24.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+float radius,area,pi=3.14;
+
+printf("Enter the radius of circle:");
+scanf("%f", &radius);
+
+area= pi*radius*radius;
+printf("The area of circle is:%f",area);
+return 0;
+}
+ 
